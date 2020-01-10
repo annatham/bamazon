fb66bc926fbd9dbbc40807a6827300098e6cb628
Anna Thammakane <anna.thammakane@gmail.com>
2020-01-10 03:39:22 -0600

    node modules

--- bamazon.sql
+++ bamazon.sql
@@ -7,8 +7,8 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR (100) NOT NULL,
     department_name VARCHAR (100) NOT NULL,
-    price DECIMAL (10, 2);
-    stock_quantity INT NOT NULL;
-    primary key (item_id)
-)
+    price DECIMAL (10, 2),
+    stock_quantity INT NOT NULL,
+    PRIMARY KEY (item_id)
+);
 
